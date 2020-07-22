a439b7200af987d35269c1e97bb2c02c9898e781
Angel Claudio <angelclaudiopr@gmail.com>
2020-07-22 08:20:59 -0400

    ac

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',  // Creation of HTML Tag here
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-site';
+  // Data binding properties
+
 }
