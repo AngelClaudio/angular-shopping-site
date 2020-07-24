bcefcb45a1a376af39dbbefe220c1bd735495d96
Angel Claudio <angelclaudiopr@gmail.com>
2020-07-24 09:41:37 -0400

    Update components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
+// A module is a bundle of functionality for the app
+// Tells angular what features the app has
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms'; // To be able to use ngModule
+ // Don't need to put .TS extension (webpack does it)
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
+// Register your component in the @NgModule, b/c Angular doesn't scan your files
+// to know which components exist, hence register them in declarations array
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent  // TS doesn't know what this means, that's why you have to import above
   ],
   imports: [
     BrowserModule,
     FormsModule  // After import on top, append to imports array
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] // This pertains to index.html, by nature only root component is attached here
 })
 export class AppModule { }
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,13 @@
+// TS doesn't know from the start what @Component is, so you need to import it.
+// We use curly braces since it could be an object with multiple elements.
+import { Component } from '@angular/core';
+
+// add decorator to enhance, and let Angular know it's a component
+@Component({// pass js object {} to configure with meta data
+  selector: 'app-server',
+  templateUrl: './server.component.html' // must give a path, so use ./ for relative path
+})
+
+export class ServerComponent {
+
+}
