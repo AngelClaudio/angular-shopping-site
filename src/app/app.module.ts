dce1c5fe6b25d4d9509c659cc5bc810a03a971c2
Angel Claudio <angelclaudiopr@gmail.com>
2020-07-28 22:37:35 -0400

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import {FormsModule} from '@angular/forms'; // To be able to use ngModule
  // Don't need to put .TS extension (webpack does it)
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 // Register your component in the @NgModule, b/c Angular doesn't scan your files
 // to know which components exist, hence register them in declarations array
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent  // TS doesn't know what this means, that's why you have to import above
+    ServerComponent,
+    ServersComponent  // TS doesn't know what this means, that's why you have to import above
   ],
   imports: [
     BrowserModule,
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+// Notice Component is imported above b/c again, TS doesn't know natively what @blah is
+@Component({
+  selector: 'app-servers',
+  template: '<app-server></app-server><app-server></app-server>', // This property you must have at all times
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
